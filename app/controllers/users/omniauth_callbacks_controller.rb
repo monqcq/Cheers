d24ec9daa9631def7a7e00f6f2d1dc9e553eeb36
monqcq <takuyaaaan23@gmail.com>
2021-05-09 08:16:29 +0000

    [add]SNS認証の処理をコントローラーに記述

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,37 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  
+  def facebook
+    callback_for(:facebook)
+  end
+  
+  def google_oauth2
+    callback_for(:google)
+  end
+  
+  def failure
+    redirect_to root_path and return
+  end
+  
+  private
+  
+  def callback_for(provider)
+    # APIから取得したユーザー情報はrequest.env['omniauth.auth']に格納されている
+    @omniauth = request.env['omniauth.auth']
+    # User.find_oauthはモデルで定義
+    info = User.find_oauth(@omniauth)
+    # deviseのヘルパーを使うために@userに代入
+    @user = info[:user]
+    # ユーザー登録済みの処理
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_massege(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    # ユーザー未登録の処理
+    else
+      @sns = info[:sns]
+      render template: "devise/registrations/new"
+    end
+  end
+  
 end
