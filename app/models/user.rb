b8d60bd4478d971d2dcb638303275e14784c3335
monqcq <takuyaaaan23@gmail.com>
2021-05-09 06:46:18 +0000

    [add]OmniAuthを使うためのオプションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+        # OmniAuthを使うためのオプション
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+         
+  has_many :sns_credential, dependent: :destroy
+  
 end
