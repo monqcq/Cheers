e884ce3b6c8a2bdaf2bf770540e241c00d51c5ca
monqcq <takuyaaaan23@gmail.com>
2021-05-15 03:15:05 +0000

    [add]updateアクション定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,13 @@ class PostsController < ApplicationController
       render :edit
     else
       redirect_to posts_path
+    end
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   private
