8f2e5d047e065d04616bbf52b8f2ca7a0a98246b
monqcq <takuyaaaan23@gmail.com>
2021-05-16 05:47:37 +0000

    [add]いいねボタン追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   # 投稿画像をアップロードするための記述
   attachment :image
-  
+
 end
