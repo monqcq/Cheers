bd4b5bc7d07474cbf27b689fb031608e08f920da
monqcq <takuyaaaan23@gmail.com>
2021-05-16 04:47:12 +0000

    [add]likeモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,6 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
