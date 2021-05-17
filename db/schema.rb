e4f69ed8233aaf91a08a984aab9f24da6f6e1e44
monqcq <takuyaaaan23@gmail.com>
2021-05-17 06:13:04 +0000

    [add]ユーザーモデルにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_063917) do
+ActiveRecord::Schema.define(version: 2021_05_17_061040) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_063917) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.integer "status", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
