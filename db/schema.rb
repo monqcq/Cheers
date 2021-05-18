5eba2760690aecb8e93a19e4cc61438d6c518a95
monqcq <takuyaaaan23@gmail.com>
2021-05-18 03:30:11 +0000

    [add]categoryモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_062343) do
+ActiveRecord::Schema.define(version: 2021_05_18_032917) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -39,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_062343) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
