9f32fbb17d942a8d46baacd002f14a269cf12e4f
monqcq <takuyaaaan23@gmail.com>
2021-05-18 06:53:41 +0000

    [add]sceneモデル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :sce
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
