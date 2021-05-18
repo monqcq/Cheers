56f464e82812e556c5ff8f23ed515dd23dfbeef3
monqcq <takuyaaaan23@gmail.com>
2021-05-18 08:35:35 +0000

    [add]検索フォームをヘッダーに設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   protected
+  
+  def set_search
+    @search = Post.ransack(params[:q])
+    @search_posts = @search.result
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
