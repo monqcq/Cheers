88e768cc1786e00fada311c3f48bdb2c7e553820
monqcq <takuyaaaan23@gmail.com>
2021-05-18 04:32:47 +0000

    [add]カテゴリー選択ボタン設置

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
-  has_many :categories, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   # 投稿画像をアップロードするための記述
