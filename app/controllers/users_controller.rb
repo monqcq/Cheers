c5ce96ce1f14bcce14743e780f4ed6bdf3bfa417
monqcq <takuyaaaan23@gmail.com>
2021-05-20 08:00:38 +0000

    [add]my_pageのレイアウトを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    if @user == current_user
+      redirect_to my_page_users_path
+    end
   end
 
   def my_page
